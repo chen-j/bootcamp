31ed5a16d7355c7947422550f0aa8cdfb461f6c1
Chenje Katanda <chenje@ssn.co.zw>
2016-10-17 16:19:10 -0400

    array methods

---
+++ week1/exercises/day1exercises.rb
@@ -0,0 +1,50 @@
+class ProgrammingLanguage
+  attr_accessor :name, :age, :type
+  def initialize(name, age, type)
+    @name = name
+    @age = age
+    @type = type
+  end
+end
+
+ruby = ProgrammingLanguage.new("Ruby", 21, "Dynamic")
+  python = ProgrammingLanguage.new("Python", 24, "Dynamic")
+  javascript = ProgrammingLanguage.new("JavaScript", 20, "Dynamic")
+  go = ProgrammingLanguage.new("Go", 6, "Static")
+  rust = ProgrammingLanguage.new("Rust", 5, "Static")
+  swift = ProgrammingLanguage.new("Swift", 2, "Static")
+  java = ProgrammingLanguage.new("Java", 20, "Static")
+
+  array_of_languages = [ruby, python, javascript, go, rust, swift, java]
+
+  def array_printer(array)
+  array.each do | language |
+    puts "Language: #{language.name} |  Age: #{language.age} |  Type System: #{language.type}"
+  end
+end
+
+array_printer(array_of_languages)
+
+aged_languages = array_of_languages.map do |lang|
+    ProgrammingLanguage.new(lang.name,lang.age+1,lang.type)
+end
+
+puts "The programming languages aged one year are: "
+array_printer(aged_languages)
+puts ""
+
+sorted_array = array_of_languages.sort {|a,b| b.age <=> a.age}
+array_printer(sorted_array)
+
+puts ""
+without_java = array_of_languages.map {|lang| lang}
+without_java.delete_at(6)
+array_printer(without_java)
+puts ""
+puts "Array of languages"
+array_printer(array_of_languages)
+
+puts ""
+puts "Shuffled"
+shuffled_languages = sorted_array.shuffle
+array_printer(shuffled_languages)
