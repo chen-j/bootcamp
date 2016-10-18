0cb302bbae777651c1a091f6d58c4063903861ba
Chenje Katanda <chenje@ssn.co.zw>
2016-10-18 13:50:32 -0400

    Add attrs for nick's car

--- week1/day2/classes_objects/app.rb
+++ week1/day2/classes_objects/app.rb
@@ -7,13 +7,21 @@ require_relative("lib/car.rb")
 
 chenje = User.new("Chenje", "chenje@example.com", 26, "African", "male")
 
-chenje.login
-chenje.greet
+# chenje.login
+# chenje.greet
 
 
 
 nizars_car = Car.new("BMW",2016,"blue","Broom")
 nicks_car = Car.new("Porsche",2016,"white","BROOOOOM")
 
+
+nizars_car.age
+nizars_car.manuf
 nizars_car.noise
+nizars_car.paint
+
+nicks_car.age
+nicks_car.manuf
 nicks_car.noise
+nicks_car.paint
