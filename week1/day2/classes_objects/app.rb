b88381cccdef606f4f665dfb8d21abd6fa93398f
Chenje Katanda <chenje@ssn.co.zw>
2016-10-18 11:35:28 -0400

    Classes in Object Orientation

---
+++ week1/day2/classes_objects/app.rb
@@ -0,0 +1,30 @@
+# Class case - Every word starts with upper case
+# --------------------------------
+
+# class User
+#   def login
+#     puts "User is logging in"
+#   end
+#
+#   def greet
+#     puts "Hello"
+#   end
+# end
+#
+#
+# first_user = User.new
+#
+# first_user.login
+# first_user.greet
+
+
+
+class Car
+  def noise
+    puts "Broom"
+  end
+end
+
+first_car = Car.new
+
+first_car.noise
---
+++ week1/day2/git.md
@@ -0,0 +1,7 @@
+**Git** is a tool that allows groups of people to **collaborate** on the same code without getting in each other's way.
+
+It's a version control system because it keeps your code's history as a series of checkpoints or versions.
+
+Commits are the building blocks of a repo
+
+A commit is a "photo" of code uniquely identified by a *sha*
