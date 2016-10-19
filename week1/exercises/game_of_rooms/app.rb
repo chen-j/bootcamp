8423d3cfda42adf15ec4222eaa69d61168e3f1ee
Chenje Katanda <chenje@ssn.co.zw>
2016-10-19 19:29:33 -0400

    Game of rooms:Add ability to move into the next roomif exit is correct

--- week1/exercises/game_of_rooms/app.rb
+++ week1/exercises/game_of_rooms/app.rb
@@ -12,24 +12,11 @@ game.add_room(Room.new("Room 3", "E", "W"))
 game.add_room(Room.new("Room 4", "E", "W"))
 game.add_room(Room.new("Room 5" ,"E", "W"))
 
-puts "You are in #{game.rooms[pos].description}"
-puts "Entry of this room is #{game.rooms[pos].entry}"
-puts "Exit of this room is #{game.rooms[pos].exit}"
+# puts "You are in #{game.rooms[pos].description}"
+# puts "Entry of this room is #{game.rooms[pos].entry}"
+# puts "Exit of this room is #{game.rooms[pos].exit}"
 
 
-# def move
-# user_input = nil
-#   while user_input != "#{game.rooms[0].exit}"
-#         puts "There is no exit there"
-#         puts "Which way? [E,W,S,N]?"
-#         user_input = gets.chomp
-#       if user_input == "#{game.rooms[0].exit}"
-#         @pos += 1
-#         puts "You're now in #{game.rooms[1].description}"
-#       end
-#   end
-# end
-
-#move
+game.move
 
 #print "#{game.rooms[0].description} exit is #{game.rooms[0].exit}"
--- week1/exercises/game_of_rooms/lib/game.rb
+++ week1/exercises/game_of_rooms/lib/game.rb
@@ -1,5 +1,6 @@
 class Game
   attr_accessor(:rooms, :pos)
+
   def initialize
     @rooms = []
     @pos = 0
@@ -7,6 +8,20 @@ class Game
 
   def add_room(room)
     @rooms.push(room)
-  end  
+  end
+
+  def move()
+  user_input = nil
+    while pos <= @rooms.length
+          puts "You're in #{rooms[pos].description}"
+          puts "Which way? [E,W,S,N]?"
+          user_input = gets.chomp
+        if user_input == "#{rooms[pos].exit}"
+          @pos += 1
+        else
+          puts "No way there, try again?"
+        end
+    end
+  end
 
 end
