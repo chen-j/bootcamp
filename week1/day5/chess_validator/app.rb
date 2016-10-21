c0616e89c9cb9482a061fb8210adb15841326417
Chenje Katanda <chenje@ssn.co.zw>
2016-10-21 15:22:55 -0400

    Chess Validator: Consolidate BlackPawn and WhitePawn in pawn.rb

--- week1/day5/chess_validator/app.rb
+++ week1/day5/chess_validator/app.rb
@@ -3,6 +3,8 @@ require_relative("lib/king.rb")
 require_relative("lib/bishop.rb")
 require_relative("lib/knight.rb")
 require_relative("lib/queen.rb")
+require_relative("lib/pawn.rb")
+# require_relative("lib/black_pawn.rb")
 
 
 
@@ -10,8 +12,8 @@ require_relative("lib/queen.rb")
 #  ------------------------- ROOK --------------------------
 #  ---------------------------------------------------------/
 
-black_rook_left = Rook.new(1, 8, "black")
-white_rook_right = Rook.new(8, 1, "white")
+#black_rook_left = Rook.new(1, 8, "black")
+#white_rook_right = Rook.new(8, 1, "white")
 
 # puts black_rook_left.move_to(2,7)
 # puts black_rook_left.move_to(1,7)
@@ -19,17 +21,25 @@ white_rook_right = Rook.new(8, 1, "white")
 # puts black_rook_left.move_to(1,2)
 
 
-black_king = King.new(5, 1, "black")
+#black_king = King.new(5, 1, "black")
 #puts black_king.move_to(4,2)
 
 
 #black_bishop_left = Bishop.new(5, 4, "black")
 #puts black_bishop_left.move_to(3,6)
 
-black_queen = Queen.new(4, 1, "black")
-puts black_queen.move_to(5,3)
+# black_queen = Queen.new(4, 1, "black")
+# puts black_queen.move_to(5,3)
 
 
 # black_knight_left = Knight.new(2, 1, "black")
 #
 # puts black_knight_left.move_to(3,3)
+
+black_pawn = BlackPawn.new(1, 2, "black")
+
+white_pawn = WhitePawn.new(1, 5, "white")
+
+p white_pawn.move_to(1,4)  #legal
+p white_pawn.move_to(1,3)  #illegal
+p black_pawn.move_to(1,1) #illegal
---
+++ week1/day5/chess_validator/lib/pawn.rb
@@ -0,0 +1,45 @@
+class WhitePawn
+    attr_accessor(:orig_x, :orig_y, :color)
+    def initialize(orig_x, orig_y, color)
+      @orig_x = orig_x
+      @orig_y = orig_y
+      @num_moves = 0
+    end
+
+    def move_to(dest_x, dest_y)
+        if @orig_y - dest_y < 0
+          puts "ILLEGAL"
+        elsif @num_moves == 0 && @orig_x.abs == dest_x.abs && @orig_y.abs - dest_y.abs == 2
+          puts "LEGAL"
+          @num_moves +=1
+        elsif @orig_x.abs == dest_x.abs && @orig_y.abs - dest_y.abs == 1
+          puts "LEGAL"
+        else
+          puts "ILLEGAL"
+        end
+    end
+end
+
+
+
+class BlackPawn
+    attr_accessor(:orig_x, :orig_y, :color)
+    def initialize(orig_x, orig_y, color)
+      @orig_x = orig_x
+      @orig_y = orig_y
+      @num_moves = 0
+    end
+
+    def move_to(dest_x, dest_y)
+        if @orig_y - dest_y > 0
+          puts "ILLEGAL"
+        elsif @num_moves == 0 && @orig_x.abs == dest_x.abs && @orig_y.abs - dest_y.abs == 2
+          puts "LEGAL"
+          @num_moves +=1
+        elsif @orig_x.abs == dest_x.abs && @orig_y.abs - dest_y.abs == 1
+          puts "LEGAL"
+        else
+          puts "ILLEGAL"
+        end
+    end
+end
