912cfd99d39c86ea61b1ca6b67b03fe50b164a44
Chenje Katanda <chenje@ssn.co.zw>
2016-10-22 22:04:05 -0400

    Chess Validator: Beautify pawn.rb with Rubocop through Beautify plugin inside Atom editor

--- week1/day5/chess_validator/lib/pawn.rb
+++ week1/day5/chess_validator/lib/pawn.rb
@@ -7,22 +7,22 @@ class Pawn
     end
 
     def move_to(dest_x, dest_y)
-			if @color == "white"
+        if @color == 'white'
 
-				if (@orig_y == 2 && dest_y == 4 && @orig_x == dest_x) || (@orig_x == dest_x && @orig_y - dest_y == -1)
-            puts 'LEGAL'
-        else
-            puts 'ILLEGAL'
-        end
+            if (@orig_y == 2 && dest_y == 4 && @orig_x == dest_x) || (@orig_x == dest_x && @orig_y - dest_y == -1)
+                puts 'LEGAL'
+            else
+                puts 'ILLEGAL'
+            end
 
-		 elsif @color == "black"
+        elsif @color == 'black'
 
-			 if (@orig_y == 7 && dest_y == 5 && @orig_x == dest_x) || (@orig_x == dest_x && @orig_y - dest_y == 1)
-					 puts 'LEGAL'
-			 else
-					 puts 'ILLEGAL'
-			 end
+            if (@orig_y == 7 && dest_y == 5 && @orig_x == dest_x) || (@orig_x == dest_x && @orig_y - dest_y == 1)
+                puts 'LEGAL'
+            else
+                puts 'ILLEGAL'
+            end
 
-		 end
-	 end
+       end
+   end
 end
