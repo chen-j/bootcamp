9a5a3d4db181aa23e7755c271d9d6631b25627bc
Chenje Katanda <chenje@ssn.co.zw>
2016-10-24 10:46:22 -0400

    Unit Testing: Beautify string_calculator.rb

--- week2/day1/unit_testing/lib/string_calculator.rb
+++ week2/day1/unit_testing/lib/string_calculator.rb
@@ -1,14 +1,12 @@
 class StringCalculator
-	def add(string = "")
-		string_arr = string.split("")
-		string_arr.map! do |number|
-			number.to_i
-		end
-		sum_array = string_arr.reduce(:+)
-		if sum_array == nil
-			0
-		else
-			sum_array
-		end
-	end
+    def add(string = '')
+        string_arr = string.split('')
+        string_arr.map!(&:to_i)
+        sum_array = string_arr.reduce(:+)
+        if sum_array.nil?
+            0
+        else
+            sum_array
+        end
+    end
 end
