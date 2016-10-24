ff310bdfa30b00a207575b8f813672d092a0e7ad
Chenje Katanda <chenje@ssn.co.zw>
2016-10-23 23:35:07 -0400

    Chess Validator: Refactor Bishop, Pawn, Knight and Rook classes to inherit from Pieces class

--- week1/day5/chess_validator/lib/rook.rb
+++ week1/day5/chess_validator/lib/rook.rb
@@ -1,10 +1,10 @@
-class Rook
-    attr_accessor(:orig_x, :orig_y, :color)
-    def initialize(orig_x, orig_y, color)
-        @orig_x = orig_x
-        @orig_y = orig_y
-        @color = color
-    end
+class Rook < Pieces
+    # attr_accessor(:orig_x, :orig_y, :color)
+    # def initialize(orig_x, orig_y, color)
+    #     @orig_x = orig_x
+    #     @orig_y = orig_y
+    #     @color = color
+    # end
 
     def move_to(dest_x, dest_y)
         if @orig_x.abs == dest_x.abs || @orig_y.abs == dest_y.abs
