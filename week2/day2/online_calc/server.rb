108a865cb0b846b322b527a4fdfbf374de1c9f93
Chenje Katanda <chenje@ssn.co.zw>
2016-10-25 16:48:06 -0400

    Online Calculator: Add -*/ operators to server.rb

--- week2/day2/online_calc/server.rb
+++ week2/day2/online_calc/server.rb
@@ -1,6 +1,15 @@
 # server.rb
 require "sinatra"
 
+
+get "/" do
+	erb(:home)
+end
+
+
+#---------------------------
+#--------- ADD --------------
+#----------------------------
 get "/add" do
   erb(:add)
 end
@@ -9,5 +18,57 @@ post "/calculate_add" do
 	first = params[:first_number].to_f
  	second = params[:second_number].to_f
 	result = first + second
- 	"#{first} + #{second} = #{result}"
+ 	@result ="The addition of #{first} and #{second} is #{result}"
+	erb(:result)
+end
+
+#---------------------------
+#--------- ADD --------------
+#----------------------------
+
+get "/subtract" do
+  erb(:subtract)
+end
+
+post "/calculate_subtract" do
+	first = params[:first_number].to_f
+ 	second = params[:second_number].to_f
+	result = first - second
+ 	@result ="The subtraction of #{first} and #{second} is #{result}"
+	erb(:result)
+end
+
+
+#---------------------------
+#--------- ADD --------------
+#----------------------------
+
+get "/multiply" do
+  erb(:multiply)
+end
+
+post "/calculate_multiply" do
+	first = params[:first_number].to_f
+ 	second = params[:second_number].to_f
+	result = first * second
+ 	@result = "Multiplication of #{first} and #{second} is #{result}"
+	erb(:result)
+end
+
+
+#---------------------------
+#--------- ADD --------------
+#----------------------------
+
+
+get "/divide" do
+  erb(:divide)
+end
+
+post "/calculate_divide" do
+	first = params[:first_number].to_f
+ 	second = params[:second_number].to_f
+	result = first / second
+ 	@result = "Division of #{first} and #{second} is #{result}"
+	erb(:result)
 end
