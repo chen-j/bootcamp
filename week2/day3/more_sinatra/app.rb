5977826ed5b72c2cffb86e3ead5fea3f6f3ceea7
Chenje Katanda <chenje@ssn.co.zw>
2016-10-26 12:18:12 -0400

    More Sinatra: Activated sessions in app.rb and home view

--- week2/day3/more_sinatra/app.rb
+++ week2/day3/more_sinatra/app.rb
@@ -1,8 +1,17 @@
 require "sinatra"
 require "sinatra/reloader" if development?
+require "pry" if development?
+
+enable(:sessions)
 
 get "/" do
+	@drink = session[:beverage]
 	erb(:home)
+end
+
+get "/save_to_session/:thing" do
+	session[:beverage] = params[:thing]
+	redirect to("/")
 end
 
 get "/hi" do
