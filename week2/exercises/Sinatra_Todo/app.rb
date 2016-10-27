e73512cfd69197ce3f6656bbee96af0e1bf64f7b
Chenje Katanda <chenje@ssn.co.zw>
2016-10-27 09:35:56 -0400

    Testing Sinatra

--- week2/exercises/Sinatra_Todo/app.rb
+++ week2/exercises/Sinatra_Todo/app.rb
@@ -1,1 +1,14 @@
+#Inside of server.rb
+require 'sinatra'
+require 'sinatra/reloader'
+# We're going to need to require our class files
+require_relative('lib/Task.rb')
+require_relative('lib/TodoList.rb')
 
+todo_list = TodoList.new("Josh")
+todo_list.load_tasks
+
+get "/" do
+	@the_todolist = todo_list
+	erb:task_index.erb
+end
