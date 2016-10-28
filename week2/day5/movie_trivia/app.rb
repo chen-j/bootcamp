3b48335af0f488022c5da5fbcce7eeeab5de410b
Chenje Katanda <chenje@ssn.co.zw>
2016-10-28 15:26:03 -0400

    Movie Trivia: Added Search results and answer check

--- week2/day5/movie_trivia/app.rb
+++ week2/day5/movie_trivia/app.rb
@@ -8,8 +8,22 @@ get "/" do
 end
 
 
-get "/search_results" do
-	the_search = Imdb::Search.new(params[:search])
-	@movies = the_search.movies
+post "/search_results" do
+	the_search = Imdb::Search.new(params[:movie_search])
+	movie_search = the_search.movies
+	@movies = movie_search.take(9)
+	@rand_year_arr = []
+	@movies.map do |movie|
+		@rand_year_arr << movie.year
+	end
+	@rand_year = @rand_year_arr.sample
 	erb :results
+end
+
+post "/check_answer" do
+  	if (params[:movie_year]) == (params[:correct_year])
+			redirect("http://www.techzim.co.zw")
+		else
+			redirect("https://ssn.co.zw")
+		end
 end
