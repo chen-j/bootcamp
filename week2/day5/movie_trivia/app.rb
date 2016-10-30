bf9813cc856d9074c3ea5dca91a8590825e0cae9
Chenje Katanda <chenje@ssn.co.zw>
2016-10-30 16:08:42 -0400

    Movie Trivia: Add Materialize CSS & JS Library

--- week2/day5/movie_trivia/app.rb
+++ week2/day5/movie_trivia/app.rb
@@ -9,7 +9,7 @@ get "/" do
 end
 
 
-post "/search_results" do
+get "/search_results" do
 	the_search = Imdb::Search.new(params[:movie_search])
 	movie_search = the_search.movies.take(15)
 	movie_p_search = movie_search.select { |movie| movie.poster != nil }
@@ -23,9 +23,5 @@ post "/search_results" do
 end
 
 post "/check_answer" do
-  	if (params[:movie_year]) == (params[:correct_year])
-			"You got it"
-		else
-			"NOPE!!"
-		end
+  	erb :answer
 end
