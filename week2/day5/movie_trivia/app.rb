4c879e2e538eef9b75cb4a1e60bfc34947d4874c
Chenje Katanda <chenje@ssn.co.zw>
2016-10-31 08:22:15 -0400

    Start Week3: Front-end HTML, CSS and JS

--- week2/day5/movie_trivia/app.rb
+++ week2/day5/movie_trivia/app.rb
@@ -4,6 +4,9 @@ require "sinatra/reloader"
 require "imdb"
 
 
+@score = 0
+
+
 get "/" do
 	erb :home
 end
@@ -11,9 +14,9 @@ end
 
 get "/search_results" do
 	the_search = Imdb::Search.new(params[:movie_search])
-	movie_search = the_search.movies.take(15)
+	movie_search = the_search.movies.take(25)
 	movie_p_search = movie_search.select { |movie| movie.poster != nil }
-	@movies = movie_p_search.take(9)
+	@movies = movie_p_search.take(15)
 	@rand_year_arr = []
 	@movies.map do |movie|
 		@rand_year_arr << movie.year
