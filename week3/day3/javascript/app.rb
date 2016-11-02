bb0f85f4e1bf434315fbc6b16edc1d5655ae2633
Chenje Katanda <chenje@ssn.co.zw>
2016-11-02 09:43:54 -0400

    Country counter in Ruby

---
+++ week3/day3/javascript/app.rb
@@ -0,0 +1,10 @@
+countries = ["Puerto Rico", "USA", "Nicaragua", "France", "Zimbabwe", "Mongolia", "Argentina", "Cuba", "Nigeria", "Haiti"]
+
+puts ""
+puts "Countries represented in this room:"
+
+countries.each do |a_country|
+	puts "Some of us are from #{a_country}"
+end
+
+puts ""
