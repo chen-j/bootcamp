6de86d4e8c7ca11ea1b21cda20f67f7e58ab5865
Chenje Katanda <chenje@ssn.co.zw>
2016-11-02 14:59:56 -0400

    Add Javascript-koans

--- week3/day3/colon/app.js
+++ week3/day3/colon/app.js
@@ -2,5 +2,7 @@ var avgColon = require('./lib/averageColon.js');
 
 
 var numbers = "80:70:90:100";
-console.log(avgColon(numbers) === 85 );
+var numbersTwo = "80&70&90&100";
+console.log(avgColon(numbers) === 85);
+console.log(avgColon(numbersTwo) === 85);
 console.log(avgColon());
--- week3/day3/colon/lib/averageColon.js
+++ week3/day3/colon/lib/averageColon.js
@@ -1,17 +1,17 @@
 function averageColon(numbersString) {
-	if (numbersString === undefined){
-		return 0;
-	} else {
-	var arrayNumbers = numbersString.split(/\D/);
-	for (var i = 0; i < arrayNumbers.length; i++) {
-		arrayNumbers[i] = Number(arrayNumbers[i]);
-	};
-	var sum = arrayNumbers.reduce(function(a,b){
-	 	return a+b;
-	});
-  var avg = sum/arrayNumbers.length;
-	return avg;
-	};
+    if (numbersString === undefined) {
+        return 0;
+    } else {
+        var arrayNumbers = numbersString.split(/\D/);
+        for (var i = 0; i < arrayNumbers.length; i++) {
+            arrayNumbers[i] = Number(arrayNumbers[i]);
+        };
+        var sum = arrayNumbers.reduce(function(a, b) {
+            return a + b;
+        });
+        var avg = sum / arrayNumbers.length;
+        return avg;
+    };
 };
 
 module.exports = averageColon;
