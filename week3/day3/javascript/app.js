4ac7bff5f074e520de46f6e6ae4315ba1164aeb5
Chenje Katanda <chenje@ssn.co.zw>
2016-11-02 11:15:00 -0400

    Country Counter in JS

--- week3/day3/javascript/app.js
+++ week3/day3/javascript/app.js
@@ -1,5 +1,30 @@
+function countCountries(country, array) {
+	var counter = 0;
+
+	array.forEach(function(eachCountry) {
+		if (eachCountry === country){
+			counter +=1;
+		}
+	});
+	return counter;
+}
+
+
+var countries = ["Puerto Rico","Puerto Rico",
+	"USA","USA","USA","USA","USA",
+	"Nicaragua", "France", "Zimbabwe", "Mongolia", "Argentina",
+	"Cuba","Cuba","Cuba","Cuba",
+	"Nigeria", "Haiti"]
+
+
+
+console.log(countCountries("USA", countries) === 5);
+console.log(countCountries("Puerto Rico", countries) === 2);
+console.log(countCountries("Cuba", countries) === 4);
+console.log(countCountries("Nicaragua", countries) === 1);
+console.log(countCountries("Japan", countries) === 0);
+
 // countries = ["Puerto Rico", "USA", "Nicaragua", "France", "Zimbabwe", "Mongolia", "Argentina", "Cuba", "Nigeria", "Haiti"]
-var countries = ["Puerto Rico", "USA", "Nicaragua", "France", "Zimbabwe", "Mongolia", "Argentina", "Cuba", "Nigeria", "Haiti"];
 
 // puts ""
 // puts "Countries represented in this room:"
--- week3/day3/javascript/app.rb
+++ week3/day3/javascript/app.rb
@@ -1,4 +1,27 @@
-countries = ["Puerto Rico", "USA", "Nicaragua", "France", "Zimbabwe", "Mongolia", "Argentina", "Cuba", "Nigeria", "Haiti"]
+def count_countries(country, array)
+	counter = 0
+	array.each do |each_country|
+		if each_country == country
+			counter +=1
+		end
+	end
+	counter
+end
+
+
+countries = ["Puerto Rico","Puerto Rico",
+	"USA","USA","USA","USA","USA",
+	"Nicaragua", "France", "Zimbabwe", "Mongolia", "Argentina",
+	"Cuba","Cuba","Cuba","Cuba",
+	"Nigeria", "Haiti"]
+
+
+p count_countries("USA", countries) == 5
+p count_countries("Puerto Rico", countries) == 2
+p count_countries("Cuba", countries) == 4
+p count_countries("Nicaragua", countries) == 1
+p count_countries("Japan", countries) == 0
+
 
 puts ""
 puts "Countries represented in this room:"
