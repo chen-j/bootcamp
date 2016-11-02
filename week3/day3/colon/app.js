aa015423b29cec9bd8df73540c69a2ca575ee709
Chenje Katanda <chenje@ssn.co.zw>
2016-11-02 14:10:18 -0400

    Refactor Colon Average

--- week3/day3/colon/app.js
+++ week3/day3/colon/app.js
@@ -1,15 +1,5 @@
-function averageColon(numbersString) {
-	var arrayNumbers = numbersString.split(':');
-	for (var i = 0; i < arrayNumbers.length; i++) {
-		arrayNumbers[i] = +arrayNumbers[i];
-	};
-	var sum = arrayNumbers.reduce(function(a,b){
-	 	return a+b;
-	});
-  var avg = sum/arrayNumbers.length;
-	return avg;
-};
+var avgColon = require('./lib/averageColon.js');
 
 
 var numbers = "80:70:90:100";
-console.log(averageColon(numbers) == 85);
+console.log(avgColon(numbers) === 85 );
---
+++ week3/day3/colon/lib/averageColon.js
@@ -0,0 +1,13 @@
+function averageColon(numbersString) {
+	var arrayNumbers = numbersString.split(/\D/);
+	for (var i = 0; i < arrayNumbers.length; i++) {
+		arrayNumbers[i] = Number(arrayNumbers[i]);
+	};
+	var sum = arrayNumbers.reduce(function(a,b){
+	 	return a+b;
+	});
+  var avg = sum/arrayNumbers.length;
+	return avg;
+};
+
+module.exports = averageColon;
