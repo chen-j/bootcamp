9e446c2034d302285aae179ff317f5dbe99bf3f9
Chenje Katanda <chenje@ssn.co.zw>
2016-11-03 11:36:52 -0400

    setTimeout example in callbacks

---
+++ week3/day4/callbacks/app.js
@@ -0,0 +1,14 @@
+"use strict"
+
+console.log("");
+console.log("START OF PROGRAM");
+
+function after5Seconds () {
+	console.log("5 seconds");
+}
+
+setTimeout (after5Seconds, 5000);
+
+console.log("");
+console.log("END OF PROGRAM");
+console.log("");
