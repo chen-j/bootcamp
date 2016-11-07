ef97e913b55f2fbacfa986f2da3ce9ef1050da8d
Chenje Katanda <chenje@ssn.co.zw>
2016-11-07 18:12:20 -0500

    Word Central: Word Frequency

--- week4/day1/wordcentral/app/controllers/text_inspections_controller.rb
+++ week4/day1/wordcentral/app/controllers/text_inspections_controller.rb
@@ -7,7 +7,16 @@ class TextInspectionsController < ApplicationController
 		@text = params[:text_inspection][:user_text]
 		@word_count = @text.split(" ").length
 		@time_reading = @word_count * 60/275
-
+		@words = @text.split(" ")
+		@frequencies = Hash.new(0)
+		@overused = []
+		@words.each do |word|
+			@frequencies[word] += 1
+		end
+		@sorted = @frequencies.sort_by{ |word, frequency| -frequency }
+		@sorted.each do |x|
+			@overused.push(x[0])
+		end
 		render "results"
 	end
 
