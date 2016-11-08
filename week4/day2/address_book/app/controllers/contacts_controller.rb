6f3fe6aba8eeb5b6b763b0537ef5a56fc77ac8d0
Chenje Katanda <chenje@ssn.co.zw>
2016-11-08 17:11:39 -0500

    Add Functionality upto Iteration 5

---
+++ week4/day2/address_book/app/controllers/contacts_controller.rb
@@ -0,0 +1,20 @@
+class ContactsController < ApplicationController
+	def index
+		@contacts = Contact.order(name: :asc)
+		render "index"
+	end
+
+	def new
+		render "new"
+	end
+
+	def create
+		contact = Contact.new(
+					:name => params[:contact][:name],
+					:address => params[:contact][:address],
+					:phone_number => params[:contact][:phone],
+					:email => params[:contact][:email])
+		contact.save
+		redirect_to("/")
+	end
+end
--- week4/day2/address_book/db/seeds.rb
+++ week4/day2/address_book/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+contactA = Contact.create(name:"Chenje", address:"25 Harare, Zimbabwe", phone_number:"+0000222002", email:"chenje@example.com")
+contactB = Contact.create(name:"Elliot", address:"22, San Francisco, USA", phone_number:"+0000222002", email:"elli@example.com")
+contactC = Contact.create(name:"Brian", address:"20, Cape Town, South Africa", phone_number:"+0000222002", email:"brian@example.com")
+contactD = Contact.create(name:"Peter", address:"43, Sofia, Bulgaria", phone_number:"+0000222002", email:"peter@example.com")
+contactE = Contact.create(name:"Thomas", address:"19, Paris, France", phone_number:"+0000222002", email:"tom@example.com")
