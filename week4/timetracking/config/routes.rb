9bc0554556225f7098531030f216ec928e2f3bfa
Chenje Katanda <chenje@ssn.co.zw>
2016-11-10 15:37:50 -0500

    Refactor with resources

--- week4/timetracking/config/routes.rb
+++ week4/timetracking/config/routes.rb
@@ -3,25 +3,9 @@ Rails.application.routes.draw do
 
 	get "/", to: "site#home"
 
-	get "/projects", to: "projects#index"
-
-	get "/projects/new", to: "projects#new"
-
-	post "/projects", to: "projects#create"
-
-	get "/projects/:id", to: "projects#show", as: "project"
-
-	get "/projects/:project_id/time_entries", to: "time_entries#index", as: "project_time_entries"
-
-	get "/projects/:project_id/time_entries/new", to: "time_entries#new", as: "new_project_time_entry"
-
-	post "/projects/:project_id/time_entries", to: "time_entries#create"
-
-	get "/projects/:project_id/time_entries/:id/edit", to: "time_entries#edit", as: "edit_project_time_entry"
-
-	patch "/projects/:project_id/time_entries/:id", to: "time_entries#update", as: "project_time_entry"
-
-    delete "/projects/:project_id/time_entries/:id", to: "time_entries#destroy"
+    resources :projects, only:[:index, :show, :new, :create] do
+        resources :time_entries, except: [:show]
+    end
 
 	get "/contact", to: "site#contact"
 
