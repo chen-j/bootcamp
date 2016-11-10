7f103b27b00f60a3451697c6641a7d62328afc6f
Chenje Katanda <chenje@ssn.co.zw>
2016-11-10 14:01:56 -0500

    Deleting and Updating modes, time

--- week4/timetracking/config/routes.rb
+++ week4/timetracking/config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
 
 	post "/projects/:project_id/time_entries", to: "time_entries#create"
 
+	get "/projects/:project_id/time_entries/:id/edit", to: "time_entries#edit", as: "edit_project_time_entry"
+
+	patch "/projects/:project_id/time_entries/:id", to: "time_entries#update", as: "project_time_entry" 
+
 	get "/contact", to: "site#contact"
 
 	get "/say_name/:name", to: "site#say_name"
