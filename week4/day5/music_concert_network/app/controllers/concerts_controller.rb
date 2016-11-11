ea0c234f49094f9822e630d640c0184c654b40fc
Chenje Katanda <chenje@ssn.co.zw>
2016-11-11 16:13:24 -0500

    Form Styling, Concerts Contoller

--- week4/day5/music_concert_network/app/controllers/concerts_controller.rb
+++ week4/day5/music_concert_network/app/controllers/concerts_controller.rb
@@ -2,24 +2,29 @@ class ConcertsController < ApplicationController
 
     def index
       @concerts = Concert.all
+      @concerts_today = Concert.all
       render "index"
     end
 
+    def show
+      @concert = Concert.find(params[:id])
+      render "show"
+    end
+
     def new
-		@new_concert = Concert.new
-		render "new"
-	end
+        @new_concert = Concert.new
+        render "new"
+    end
 
     def create
-        @new_concert = Concert.new(
-            :artist => params[:concert][:artist],
-            :venue => params[:concert][:venue],
-            :city => params[:concert][:city],
-            :date => params[:concert][:date],
-            :description => params[:concert][:description],
-            :price => params[:concert][:price])
+        @new_concert = Concert.new(entry_params)
         @new_concert.save
+        redirect_to "/"
+    end
 
-        redirect_to :action => "index"
+    private
+
+    def entry_params
+        params.require(:concert).permit(:artist, :venue, :city, :date, :description, :price)
     end
 end
