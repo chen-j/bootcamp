f05890530d90c207f59f1302b1d42635670c8d52
Chenje Katanda <chenje@ssn.co.zw>
2016-11-11 11:19:12 -0500

    Build new&create concert action as well as accompanying form

--- week4/day5/music_concert_network/app/controllers/concerts_controller.rb
+++ week4/day5/music_concert_network/app/controllers/concerts_controller.rb
@@ -1,6 +1,25 @@
 class ConcertsController < ApplicationController
+
     def index
       @concerts = Concert.all
       render "index"
+    end
+
+    def new
+		@new_concert = Concert.new
+		render "new"
+	end
+
+    def create
+        @new_concert = Concert.new(
+            :artist => params[:concert][:artist],
+            :venue => params[:concert][:venue],
+            :city => params[:concert][:city],
+            :date => params[:concert][:date],
+            :description => params[:concert][:description],
+            :price => params[:concert][:price])
+        @new_concert.save
+
+        redirect_to :action => "index"
     end
 end
