ce2e32e6de4d682c0ffe1f5b103e36c7ab4b78a4
Chenje Katanda <chenje@ssn.co.zw>
2016-11-14 11:28:13 -0500

    Intro to jQuery selectors

---
+++ week5/day1/jquery/js/app.js
@@ -0,0 +1,10 @@
+console.log("CONSOLE LOG");
+
+$(document).ready(function() {
+
+    $(".js-button-thing").on("click", function(){
+        $(".container").append (`<img src="images/unicorns1.gif">`)
+    });
+
+
+});
