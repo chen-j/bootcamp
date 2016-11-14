d21b15c8bb9472abb41b017daa35d48d43c30876
Chenje Katanda <chenje@ssn.co.zw>
2016-11-14 14:02:39 -0500

    jQuery events

--- week5/day1/jquery/js/app.js
+++ week5/day1/jquery/js/app.js
@@ -2,9 +2,49 @@ console.log("CONSOLE LOG");
 
 $(document).ready(function() {
 
-    $(".js-button-thing").on("click", function(){
-        $(".container").append (`<img src="images/unicorns1.gif">`)
+    $(".js-pizza").on("click", function() {
+        $(".empanadas").remove();
     });
 
+    $(".empanadas").click(function (){
+        $(".js-heading").append(`
+            <span> EMPANADA 😋 TIME </span>
+            <a href=#>SEE MORE EMPANADAS</a>
+            `);
+    });
+
+    $("#cookies").on("click", cookieMessage);
+
+
+
+    $(".js-button-thing").on("click", function(){
+        $(".container").prepend (`<img src="images/unicorns1.gif">`)
+
+    $("#ice-cream").on("click", iceCreamMessage);
+
+    });
 
 });
+
+
+function iceCreamMessage() {
+    var content = `<i>Ice Cream🍦</i>`;
+    $(".js-jq-paragraph").html(content);
+
+};
+
+
+function cookieMessage() {
+    var cookieContent = `
+    <div class="popup">
+        <h2>🍪 Have some cookies 🍪</h2>
+        <p>Cookies are good for you. Here's why:</p>
+        <ul>
+            <li>Cookies taste good.</li>
+            <li>They have 0 calories.</li>
+            <li>Cookies don't lie</li>
+        </ul>
+    </div>
+    `;
+    $(".container").append(cookieContent);
+};
