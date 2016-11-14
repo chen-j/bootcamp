c724f059d761979d0dfeab471aa411b4fa417522
Chenje Katanda <chenje@ssn.co.zw>
2016-11-13 21:10:08 -0500

    Style concert page

--- week4/day5/music_concert_network/app/models/concert.rb
+++ week4/day5/music_concert_network/app/models/concert.rb
@@ -1,5 +1,5 @@
 class Concert < ApplicationRecord
     has_many :comments
-    has_attached_file :poster, :styles => { :medium => "300x440>", :thumb => "100x150#" }, :default_url => "/images/:style/poster.jpg"
+    has_attached_file :poster, :styles => { :medium => "250x368>", :thumb => "100x150#" }, :default_url => "/images/:style/poster.jpg"
     validates_attachment_content_type :poster, :content_type => /\Aimage\/.*\Z/
 end
