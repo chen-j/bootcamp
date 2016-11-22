d23f8ff4de2ab4b7735aa11bbb662b10770646a3
Chenje Katanda <chenje@ssn.co.zw>
2016-11-22 12:02:50 -0500

    Add authentication

---
+++ week6/day2/auth-ex/app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+    def new
+      render :new
+    end
+
+    def create
+      user_maybe = User.find_by(email: params[:email])
+      if user_maybe == nil || user_maybe.authenticate(params[:password]) == false
+          redirect_to "/login"
+      else
+          session[:user_id] = user_maybe.id
+          redirect_to "/"
+      end
+    end
+end
