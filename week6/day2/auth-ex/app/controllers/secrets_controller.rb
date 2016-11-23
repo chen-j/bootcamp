c445472b24bcc727fe27024a0641e19f87293177
Chenje Katanda <chenje@ssn.co.zw>
2016-11-23 12:41:56 -0500

    Refactor authorization for SecretsController

--- week6/day2/auth-ex/app/controllers/secrets_controller.rb
+++ week6/day2/auth-ex/app/controllers/secrets_controller.rb
@@ -1,11 +1,7 @@
 class SecretsController < ApplicationController
+    before_action :check_if_admin
+
     def show
-        get_current_user
-        if session[:user_id] && @current_user.role == "admin"
         render :show
-        else
-        flash[:admin_only] = "You need to be admin to learn our secrets"
-        redirect_to "/"
-        end
     end
 end
