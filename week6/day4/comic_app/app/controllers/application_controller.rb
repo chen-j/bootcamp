665f5272906b98cbd231b9eb7e7af73a39099667
Chenje Katanda <chenje@ssn.co.zw>
2016-11-28 11:42:44 -0500

    start devise customization

--- week6/day4/comic_app/app/controllers/application_controller.rb
+++ week6/day4/comic_app/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :add_name_to_devise, if: :devise_controller?
+
+
+  def add_name_to_devise
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+  end
+
 end
