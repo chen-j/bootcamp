3643e1fa897c42ef6c59a248527d6b934e473b79
Chenje Katanda <chenje@ssn.co.zw>
2016-11-28 12:00:07 -0500

    generate seeds for users and a couple of comics each

--- week6/day4/comic_app/app/models/user.rb
+++ week6/day4/comic_app/app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
     validates :first_name, presence: true
-    
+
+    has_many :comics
+
 end
--- week6/day4/comic_app/db/seeds.rb
+++ week6/day4/comic_app/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+clark = User.create!(first_name: "Clark", email: "clark@dailyplanet.com", password: "whatever", password_confirmation: "whatever")
+tony = User.create!(first_name: "Tony", email: "tony@stark.com", password: "whatever", password_confirmation: "whatever")
+rick = User.create!(first_name: "Rick", email: "rick@zombies.co", password: "whatever", password_confirmation: "whatever")
+
+
+clark.comics.create!(title: "Watchmen", publisher: "Vertigo")
+clark.comics.create!(title: "Walking Dead", publisher: "Planeta")
+
+rick.comics.create!(title: "V for Vendetta", publisher: "Vertigo")
+rick.comics.create!(title: "300", publisher: "Norma")
+
+tony.comics.create!(title: "The unbeatable Squirrel Girl", publisher: "Marvel")
+tony.comics.create!(title: "Thunderbolts", publisher: "Marvel")
